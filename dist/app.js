af611d46b574508af04c66eecc68a716071ed326
Bruna <bruna.dall9741@gmail.com>
2023-02-03 14:53:40 -0300

    fix: cors

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 require("express-async-errors");
 require("reflect-metadata");
 const routes_1 = require("./routes");
 const handleError_middleware_1 = __importDefault(require("./middlewares/handleError.middleware"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: '*'
+}));
 (0, routes_1.appRoutes)(app);
 app.use(handleError_middleware_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from 'cors'
 import "express-async-errors"
 import "reflect-metadata"
 
@@ -7,6 +8,9 @@ import handleErrorMiddleware from "./middlewares/handleError.middleware"
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: '*'
+}));
 
 appRoutes(app)
 app.use(handleErrorMiddleware)
