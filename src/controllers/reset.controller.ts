9047ad98ac780c0b447ffadf8da62071df2ac642
brunadalla <bruna.dall@hotmail.com>
2025-08-11 15:42:12 -0300

    fix: return "OK" response in resetController

--- src/controllers/reset.controller.ts
+++ src/controllers/reset.controller.ts
@@ -3,7 +3,7 @@ import resetService from "../services/reset.service"
 
 const resetController = async (req: Request, res: Response) => {
   await resetService()
-  return res.status(200).send()
+  return res.status(200).send("OK")
 }
 
 export default resetController
